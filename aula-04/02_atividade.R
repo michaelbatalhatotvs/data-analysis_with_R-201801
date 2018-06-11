e50e3d8fbba89a89e9b79b6cec3409f7275a663f
michaelbatalhatotvs <michael.ferreira@totvs.com.br>
2018-06-10 21:15:24 -0300

    exercicios.

--- aula-04/02_atividade.R
+++ aula-04/02_atividade.R
@@ -1,24 +1,50 @@
 library(tidyverse)
 library(lubridate)
+library(dplyr)
 
-## Nesta atividade você deve utilizar o resultado do exercício 01 da Atividade da aula 03 (remuneração em dólares convertida para reais)
+## Nesta atividade você deve utilizar o resultado do exercício 01 da Atividade da aula 03
+##(remuneração em dólares convertida para reais)
 ## Utilize o código daquele exercício como ponto de partida para esta atividade. 
 ## Sempre utilize o caminho relativo, não o caminho absoluto, pois não funcionará na correção do exercício.
 
 ### IMPORTANTE ###
-## Se você utilizar alguma função própria ou do material de aula, o código da(s) função(ões) deve estar neste arquivo da atividade.
+## Se você utilizar alguma função própria ou do material de aula, 
+#o código da(s) função(ões) deve estar neste arquivo da atividade.
+
+salarios <- read_csv("aula-03/data/201802_dados_salarios_servidores.csv.gz")
 
 
+###ALUNO MICHAEL BATALHA CORDEIRO FERREIRA
 ### 1 ####
 ## 
 ## Correlação de ano de ingresso por cargo
-## - Determine o coeficiente de correlação entre o tempo em anos desde a DATA_INGRESSO_ORGAO e o tempo em anos desde a DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO
+## - Determine o coeficiente de correlação entre o tempo em anos desde a DATA_INGRESSO_ORGAO 
+##  e o tempo em anos desde a DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO
 ##   para todos os cargos que possuem no mínimo 200 servidores.
-## - Crie uma coluna que determina se a correlação é positiva ou negativa, e outra coluna que define a força da correlação de acordo com 
+## - Crie uma coluna que determina se a correlação é positiva ou negativa, e outra coluna que 
+##  define a força da correlação de acordo com 
 ##   o material visto em aula sobre interpretação do coeficiente.
-## - O resultado desta atividade deve ser um Data Frame com as variáveis de Cargo, Coeficiente de Correlação, Direção da Correlação e Força da Correlação
+## - O resultado desta atividade deve ser um Data Frame com as variáveis de Cargo, Coeficiente 
+##   de Correlação, Direção da Correlação e Força da Correlação
 ## 
 ### # ####
+subset_salarios %>%
+  group_by(DESCRICAO_CARGO) %>%
+  summarise( SERVIDORES = n(),COEFI_COR = cor(x = ( 2018 - year (DATA_INGRESSO_ORGAO)), 
+                                              y = (2018 -year(DATA_DIPLOMA_INGRESSO_SERVICOPUBLICO))))%>%
+  ungroup()%>%
+  filter(SERVIDORES >=200)%>%
+  arrange(SERVIDORES)%>%
+  mutate(DIR = (ifelse(COEFI_COR>0,'POS','NEG')))%>%
+  mutate(CORRELACAO_ABSOLUTA = (ifelse(COEFI_COR>0,COEFI_COR,(COEFI_COR * (-1)))))%>%
+  mutate(FORCA = ifelse(CORRELACAO_ABSOLUTA >=0.9, '5',
+                        ifelse(CORRELACAO_ABSOLUTA >= 0.7 & CORRELACAO_ABSOLUTA < 0.9, '4',    
+                               ifelse(CORRELACAO_ABSOLUTA >= 0.5 & CORRELACAO_ABSOLUTA < 0.7, '3',
+                                      ifelse(CORRELACAO_ABSOLUTA >= 0.3 & CORRELACAO_ABSOLUTA < 0.5, '2','1')))))%>%
+  select(DESCRICAO_CARGO, COEFI_COR, DIR, FORCA, CORRELACAO_ABSOLUTA) -> CORRELACAO_DE_ANO_EXE1
+
+CORRELACAO_DE_ANO_EXE1%>%
+  select(DESCRICAO_CARGO, COEFI_COR, DIR, FORCA)
 
 ### 2 ###
 ##
